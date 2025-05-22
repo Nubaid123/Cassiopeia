0e5099ca7d5699bc0bc0b04f0cd0f307688fc716
Nubaid123 <116947437+Nubaid123@users.noreply.github.com>
2025-05-22 23:02:35 +0300

    Update Pevent.js

--- screens/Pevent.js
+++ screens/Pevent.js
@@ -30,7 +30,11 @@ export default function Pevents({ navigation }) {
       <View style={{ alignItems: 'center', marginTop: 20 }}>
         <EventCard
           image={e}
-          onPress={() => navigation.navigate('PeventD')} // confirm your screen name
+          onPress={() => {
+  console.log("Pressed!");
+  navigation.navigate('PeventD');
+}}
+
           text="Under The Night Sky"
           date="25/4/2025"
           style={{ width: sWidth }}
